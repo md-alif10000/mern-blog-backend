1e0836ff8ca3b068af44d2a483c2cd1190e687fc
md_alif <md.alif10000@gmail.com>
2021-05-05 17:41:04 +0600

    removed nodemon

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ connect();
 
 // App config
 app.use(express.json());
-app.use(cors())
 app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
 
 // Router import
 const userRouter = require("./routes/userRoutes");
